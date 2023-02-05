37cf5532158f971b8139fdd81d9260e286350b6a
caldwell-63 <johnnydevx@proton.me>
2023-02-05 16:55:20 +0100

    clean window separators, more gitsigns hl groups

--- colors/vs-dark.lua
+++ colors/vs-dark.lua
@@ -38,13 +38,13 @@ api.applyHlGroups {
   DiagnosticInfo = { colors.fg },
   DiagnosticHint = { '#fff000' },
   DiagnosticError = { '#f00000' },
-
   ColorColumn = { colors.float },
+
   Search = { colors.float },
   CurSearch = { colors.visual },
   IncSearch = { colors.secondary },
-  Conceal = { colors.primary, '#ff0000' },
 
+  Conceal = { colors.primary, '#ff0000' },
   Identifier = { nil, 'fg' },
   Statement = { nil, colors.primary },
   Boolean = { nil, colors.primary },
@@ -64,7 +64,7 @@ api.applyHlGroups {
   -- ???
   -- Scrollbar = { r, g },
   -- Tooltip = { g, b },
-  WinSeparator = { '#f00000', '#00ff00' },
+  WinSeparator = { colors.bg, colors.float },
 
   Normal = { colors.bg, colors.fg },
   NormalNC = { colors.bg, colors.fg }, -- Not Active
@@ -84,8 +84,12 @@ api.applyHlGroups {
 };
 
 local gitsigns = {
-  GitSignsChange = { colors.bg, '#47ebeb' },
   GitSignsAdd = { colors.bg, '#68eb47' },
+  GitSignsChange = { colors.bg, '#47ebeb' },
+  GitSignsDelete = { colors.bg, '#47ebeb' },
+  GitSignsChangedelete = { colors.bg },
+  GitSignsTopdelete = { colors.bg },
+  GitSignsUntracked = { colors.bg },
   GitSignsAddLn = { colors.bg, '#6847eb' },
 };
 
