720bb2da1bfecd7bf9c09cfc21879320f6370f43
caldwell-63 <johnnydevx@proton.me>
2023-02-07 18:17:42 +0100

    reorganize some keymaps

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -7,15 +7,14 @@ local function n(l, r, desc, isSilent)
   vim.keymap.set('n', l, r, { desc = desc, silent = isSilent });
 end
 
-n('<Leader>ot', api.startNewCmd, '🏴 Open/start new cmd (terminal)', true);
+n('<Leader>ot', api.startNewCmd, 'Open/start new cmd (terminal)', true);
 n('<Leader>oc', api.startChrome, 'Open/start Chrome', true);
-n('<leader>m', mason.open, 'Open Mason');
-n('<F3>', api.goToConfig, 'Open Config');
-n('<C-j>', treesitter.printHlGroupNameUnderTheCursor);
-n('<C-p>', function() vim.cmd 'so' end);
+n('<Leader>om', mason.open, 'Open mason', true);
+n('<Leader>oo', api.goToConfig, 'Open Config');
 
 n('<Leader>gi', git.init, 'git init');
 n('<Leader>gg', git.commitAll, 'save all and git commit all');
 n('<Leader>gp', git.push, 'git push');
 
--- n('?', api.runCopiedCommand, 'Run copied command');
+n('<C-j>', treesitter.printHlGroupNameUnderTheCursor);
+n('<C-p>', function() vim.cmd 'so' end);
