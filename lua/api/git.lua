653a5d216bf13e5201d565da245d396023df17e5
caldwell-63 <johnnydevx@proton.me>
2023-02-07 19:10:14 +0100

    git silent

--- lua/api/git.lua
+++ lua/api/git.lua
@@ -5,8 +5,8 @@ return {
   commitAll = function()
     vim.cmd 'wa';
     local commitMessage = vim.fn.input('');
-    vim.cmd '!git add .';
-    vim.cmd('!git commit -a -m "' .. commitMessage .. '"');
+    vim.cmd 'silent !git add .';
+    vim.cmd('silent !git commit -a -m "' .. commitMessage .. '"');
   end,
   push = function ()
     vim.cmd 'silent !git push';
