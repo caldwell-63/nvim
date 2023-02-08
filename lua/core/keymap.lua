493099c61ed364f988dc9b927e646b0f9b9b5ae0
caldwell-63 <johnnydevx@proton.me>
2023-02-08 18:44:21 +0100

    format all

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -4,25 +4,25 @@ local treesitter = require 'api.treesitter';
 local git = require 'api.git';
 
 vim.keymap.set('n', '<Leader>ot', api.startNewCmd, {
-  desc = 'Open/start new cmd/terminal)',
-  silent  = true,
+  desc   = 'Open/start new cmd/terminal)',
+  silent = true,
 });
 
-vim.keymap.set('n', '<Leader>oc', api.startChrome,  { 
-  silent = true ,
-  desc ='Open/start Chrome',
+vim.keymap.set('n', '<Leader>oc', api.startChrome, {
+  silent = true,
+  desc = 'Open/start Chrome',
 });
 
 vim.keymap.set('n', '<Leader>om', mason.open, {
   silent = true,
-  desc= 'Open mason' 
+  desc = 'Open mason'
 });
 
-vim.keymap.set('n', '<Leader>oo', api.goToConfig, {desc='Open Config'});
+vim.keymap.set('n', '<Leader>oo', api.goToConfig, { desc = 'Open Config' });
 
-vim.keymap.set('n', '<Leader>gi', git.init, {desc='git init'});
-vim.keymap.set('n', '<Leader>gg', git.commitAll, {desc='save all and git commit all'});
-vim.keymap.set('n', '<Leader>gp', git.push, {desc='git push'});
+vim.keymap.set('n', '<Leader>gi', git.init, { desc = 'git init' });
+vim.keymap.set('n', '<Leader>gg', git.commitAll, { desc = 'save all and git commit all' });
+vim.keymap.set('n', '<Leader>gp', git.push, { desc = 'git push' });
 
 vim.keymap.set('n', '<C-j>', treesitter.printHlGroupNameUnderTheCursor);
 vim.keymap.set('n', '<C-p>', function() vim.cmd 'so' end);
