5f528fa1462dd809690fcb064371366961c8fff8
caldwell-63 <johnnydevx@proton.me>
2023-02-09 14:53:57 +0100

    lsp/lua: disable single file mode, root at .git

--- lua/lsp/lang/lua.lua
+++ lua/lsp/lang/lua.lua
@@ -1,15 +1,23 @@
 local lspc = require 'lspconfig';
 local flags = require 'lsp.common.flags';
 local on_attach = require 'lsp.common.on-attach';
+local util = require 'lspconfig.util';
 
 lspc.sumneko_lua.setup {
   -- root_dir = root_pattern(...)
   on_attach = on_attach,
   flags = flags,
-  single_file_support = true,
+  single_file_support = false,
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
   log_level = 2,
+  root_dir = function(fname)
+    local root = util.root_pattern('.git')(fname);
+
+    print(root);
+
+    return root;
+  end,
   settings = {
     Lua = {
       runtime = { version = 'LuaJIT' },
