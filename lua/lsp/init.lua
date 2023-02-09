d886b4e2c299616fe2955ea20a84358618a51483
caldwell-63 <johnnydevx@proton.me>
2023-02-09 14:47:51 +0100

    lsp typescript

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,8 +4,7 @@ require 'lsp.common.cmp';
 require 'lsp.lang.json';
 require 'lsp.lang.lua';
 require 'lsp.lang.emmet';
-
--- require 'lsp.lang.typescript';
+require 'lsp.lang.typescript';
 
 -- Todo:
 --
--- lua/lsp/lang/typescript.lua
+++ lua/lsp/lang/typescript.lua
@@ -1,8 +1,21 @@
 local lspc = require 'lspconfig';
 local flags = require 'lsp.common.flags';
 local on_attach = require 'lsp.common.on-attach';
+local capabilities = require 'cmp_nvim_lsp'.default_capabilities();
+local util = require 'lspconfig.util';
 
 lspc.tsserver.setup {
   on_attach = on_attach,
   flags = flags,
+  capabilities = capabilities,
+  single_file_support = false,
+  root_dir = function(fname)
+    -- local rd = util.root_pattern('tsconfig.json')(fname);
+    -- local alt = util.root_pattern('package.json', '.git', 'jsconfig.json')(fname);
+
+    -- this works correctly for a monorepos, not scoping to one package
+    local root = util.root_pattern('.git')(fname);
+
+    return root;
+  end,
 };
