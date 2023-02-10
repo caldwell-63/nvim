9e4a665a73ef141ddededc8f2d698d5e05e03d2e
caldwell-63 <johnnydevx@proton.me>
2023-02-10 18:05:54 +0100

    yarn fix

--- lua/lsp/lang/emmet.lua
+++ lua/lsp/lang/emmet.lua
@@ -1,4 +1,14 @@
+local capabilities = require 'cmp_nvim_lsp'.default_capabilities();
+local flags = require 'lsp.common.flags';
+local on_attach = require 'lsp.common.on-attach';
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 require 'lspconfig'.emmet_ls.setup {
+  cmd = { "emmet-ls2.cmd", "--stdio" },
+  capabilities = capabilities,
+  flags = flags,
+  on_attach = on_attach,
   filetypes = {
     'html',
     'typescript',
